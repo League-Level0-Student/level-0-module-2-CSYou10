a61792bcdd6d04c0569532de2afd4930e94d93ba
CSYou10 <league@WTS-IM-6.attlocal.net>
2019-04-22 18:25:29 -0700

    :smile:
    sketch aa = IceCream Cone
    sketch a = dot race
    Voting Booth
    IceCream and Voting COMPLETE
    Dot Race INC

--- src/methods/VotingBooth.java
+++ src/methods/VotingBooth.java
@@ -1,5 +1,18 @@
 package methods;
 
+import javax.swing.JOptionPane;
+
 public class VotingBooth {
+	public static void main(String[] args) {
+
+		String Age = JOptionPane.showInputDialog("How old are you?");
+		int intAge = Integer.parseInt(Age);
+		if (intAge >= 18) {
+JOptionPane.showInputDialog("Who do you think the next president should be?");
+		} else {
+			JOptionPane.showMessageDialog(null, "Nobody cares what you think!");
+		}
+
+	}
 
 }
